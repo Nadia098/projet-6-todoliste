32efab5948f8ba2d5088c62bbc1d9990d05693c9
Nadia098 <nachit.na@gmail.com>
2017-06-15 15:46:44 +0200

    code en groupet et commenter

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include 'contenu.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+	<form action="contenu.php" method="post">
+		<textarea name="tache" class="tache"></textarea>
+		<br>
+		<button type="submit" name="submit" class="submit">ajout</button>
+	</form>
+
+</body>
+</html>
